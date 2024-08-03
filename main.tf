68eb88770c0115cb862f5a4abaf3a8e8a9e2af85
test <test@gmail.com>
2024-08-03 21:37:51 +0530

    first change

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.61.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = var.akey
+  secret_key = var.skey
+  region     = var.location
+}
+
+terraform {
+  cloud {
+    organization = "priar"
+    workspaces {
+      name = "cli-w1"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "b1" {
+  bucket = "tc-bucket1-030824"
+
+}
+
+resource "aws_instance" "vm1" {
+  
+  ami = "ami-025fe52e1f2dc5044"
+  instance_type = "t2.nano"
+  tags = {
+    "Name" = "tc-vm1"
+  }
+}
+  
